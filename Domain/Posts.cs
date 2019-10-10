9ec6941fe8e7395c91893f886b53bd9c25c8e2e7
daniielamaro <daniel.amaro.rj@gmail.com>
2019-10-10 18:03:54 -0300

    Implementação das classes de dominio (post e user)

---
+++ Domain/Posts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public sealed class Posts
+    {
+        public readonly Guid Id;
+        public readonly User Autor;
+        public string Title { get; protected set; }
+        public string Content { get; protected set; }
+        public readonly DateTime DateCreated;
+    }
+}
---
+++ Domain/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public sealed class User
+    {
+        public Guid Id { get; protected set; }
+        public string Name { get; protected set; }
+        public DateTime Birthdate { get; protected set; }
+        public string Email { get; protected set; }
+    }
+}
