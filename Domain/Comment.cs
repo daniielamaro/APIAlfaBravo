1663df37d85607dc3e4d6364c3e9bfdc812f8514
daniielamaro <daniel.amaro.rj@gmail.com>
2019-10-12 13:40:50 -0300

    projeto base da API

---
+++ Domain/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+   public  class Comment
+   {
+        public Guid Id { get; protected set; }
+        public User Autor { get; protected set; }
+        public string Content { get; protected set; }
+   }
+}
