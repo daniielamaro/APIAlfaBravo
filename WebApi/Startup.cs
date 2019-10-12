91190ab9a9d97473ef065161d9e15df2b1160878
daniielamaro <daniel.amaro.rj@gmail.com>
2019-10-12 15:52:49 -0300

    projeto

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -28,7 +28,7 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+    
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1",
