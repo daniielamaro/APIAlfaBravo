ad57d9950319280907e8130c14adb4d3c761de4c
daniielamaro <daniel.amaro.rj@gmail.com>
2019-10-16 11:47:20 -0300

    interfaces projeto

---
+++ Domain/BusinessRules/ValidateUser.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.BusinessRules
+{
+    public sealed class ValidateUser
+    {
+        public bool Validate(User user)
+        {
+            return (
+                ValidateID.Execute(user.Id) && 
+                !IsStringEmpty.Execute(user.Name)
+            );
+        }
+    }
+}
---
+++ Infrastructure/obj/Debug/netcoreapp2.2/i5scpgx1.sql
@@ -0,0 +1,54 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" character varying(150) NOT NULL,
+    "ProductVersion" character varying(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+CREATE TABLE "Topic" (
+    "Id" uuid NOT NULL,
+    "Name" text NULL,
+    CONSTRAINT "PK_Topic" PRIMARY KEY ("Id")
+);
+
+CREATE TABLE "User" (
+    "Id" uuid NOT NULL,
+    "Name" text NULL,
+    "Birthdate" text NULL,
+    "Email" text NULL,
+    "Senha" text NULL,
+    CONSTRAINT "PK_User" PRIMARY KEY ("Id")
+);
+
+CREATE TABLE "Publications" (
+    "Id" uuid NOT NULL,
+    "AutorId" uuid NULL,
+    "Title" text NULL,
+    "Content" text NULL,
+    "DateCreated" timestamp without time zone NOT NULL,
+    "TopicId" uuid NULL,
+    CONSTRAINT "PK_Publications" PRIMARY KEY ("Id"),
+    CONSTRAINT "FK_Publications_User_AutorId" FOREIGN KEY ("AutorId") REFERENCES "User" ("Id") ON DELETE RESTRICT,
+    CONSTRAINT "FK_Publications_Topic_TopicId" FOREIGN KEY ("TopicId") REFERENCES "Topic" ("Id") ON DELETE RESTRICT
+);
+
+CREATE TABLE "Comment" (
+    "Id" uuid NOT NULL,
+    "AutorId" uuid NULL,
+    "Content" text NULL,
+    "PublicationId" uuid NULL,
+    CONSTRAINT "PK_Comment" PRIMARY KEY ("Id"),
+    CONSTRAINT "FK_Comment_User_AutorId" FOREIGN KEY ("AutorId") REFERENCES "User" ("Id") ON DELETE RESTRICT,
+    CONSTRAINT "FK_Comment_Publications_PublicationId" FOREIGN KEY ("PublicationId") REFERENCES "Publications" ("Id") ON DELETE RESTRICT
+);
+
+CREATE INDEX "IX_Comment_AutorId" ON "Comment" ("AutorId");
+
+CREATE INDEX "IX_Comment_PublicationId" ON "Comment" ("PublicationId");
+
+CREATE INDEX "IX_Publications_AutorId" ON "Publications" ("AutorId");
+
+CREATE INDEX "IX_Publications_TopicId" ON "Publications" ("TopicId");
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20191013024742_Inicial', '2.2.6-servicing-10079');
+
