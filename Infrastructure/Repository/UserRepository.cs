46aff6e5a292cbab7cfe97395713792ab03d1d1f
John-91 <jonvaladao91@gmail.com>
2019-10-16 12:21:33 -0300

    interfaces projeto

---
+++ Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using Domain.Repository;
+using System.Linq;
+
+namespace Infrastructure.Repository
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+
+        /// <summary>
+        /// Aqui entra os metodos para acesso ao DB com linq a serem chamados pelas interfaces Domain.Repository
+        /// </summary>
+        public UserRepository(ApiContext apiContext) : base(apiContext)
+        {
+
+        }
+    }
+}
