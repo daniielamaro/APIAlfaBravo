6683674e2d11f576feece93c2151205fbd58686a
daniielamaro <daniel.amaro.rj@gmail.com>
2019-10-16 13:53:45 -0300

    interfaces

---
+++ Domain/Repository/IGetRegisterById.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Repository
+{
+    public interface IGetRegisterById<TEntity>
+    {
+        TEntity SelectId(Guid id);
+    }
+}
