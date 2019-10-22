1591505fdd5ced7a087d1b29e8e8f0ebd7b06e3d
daniielamaro <daniel.amaro.rj@gmail.com>
2019-10-22 10:38:52 -0300

    atualização validação

---
+++ Application/Entity/UserRepository.cs
@@ -0,0 +1,69 @@
+﻿using Application.Repository;
+using Domain;
+using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Application.Entity
+{
+    public class UserRepository : IUserRepository
+    {
+
+        protected readonly ApiContext ApiContext;
+
+        public UserRepository()
+        {
+            ApiContext = new ApiContext();
+        }
+
+        public User Create(User user)
+        {
+            ApiContext.Users.Add(user);
+            ApiContext.SaveChanges();
+
+            return user;
+        }
+
+        public User Delete(Guid id)
+        {
+            User user = ApiContext.Users.Find(id);
+
+            List<Comment> RelatedComments = ApiContext.Comments.Where(x => x.Autor.Id == user.Id).ToList();
+            foreach (Comment comment in RelatedComments)
+            {
+                ApiContext.Comments.Remove(comment);
+            }
+
+            List<Publication> RelatedPublications = ApiContext.Publications.Where(x => x.Autor.Id == user.Id).ToList();
+            foreach(Publication publication in RelatedPublications)
+            {
+                ApiContext.Publications.Remove(publication);
+            }
+
+            ApiContext.Remove(user);
+            ApiContext.SaveChanges();
+
+            return user;
+        }
+
+        public List<User> GetAll()
+        {
+            return ApiContext.Users.ToList();
+        }
+
+        public User GetById(Guid id)
+        {
+            return ApiContext.Users.Find(id);
+        }
+
+        public User Update(User user)
+        {
+            ApiContext.Entry(user).State = EntityState.Modified;
+            ApiContext.SaveChanges();
+
+            return user;
+        }
+    }
+}
---
+++ Infrastructure/Configuration/PublicationConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain;
+
+namespace Infrastructure.Configuration
+{
+    public class PublicationConfiguration : IEntityTypeConfiguration<Publication>
+    {
+        public void Configure(EntityTypeBuilder<Publication> builder)
+        {
+            // Aqui configuramos a validates no DB
+            builder.HasKey(p => p.Id);
+
+            builder
+                .Property(p => p.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder
+                .Property(p => p.Content)
+                .IsRequired()
+                .HasMaxLength(1200);
+
+            builder
+                .Property(p => p.DateCreated)
+                .IsRequired();
+
+            builder.HasMany(p => p.Comments);
+
+        }
+    }
+}
