cad3c5565e90bf950b0e2b9820c2caf17a806d0c
daniielamaro <daniel.amaro.rj@gmail.com>
2019-10-22 10:34:38 -0300

    atualização validação

---
+++ Application/BusinessRules/UserExistValidator.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.BusinessRules
+{
+    public class UserExistValidator : AbstractValidator<User>
+    {
+        public UserExistValidator()
+        {
+            RuleFor(x => x.Id)
+                .Must(VerifyUser)
+                .WithMessage("Este usuario não existe.");
+        }
+
+        private bool VerifyUser(Guid id)
+        {
+
+        }
+    }
+}
