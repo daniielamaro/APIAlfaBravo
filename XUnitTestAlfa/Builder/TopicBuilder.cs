710af8c2d4998e3f56fb69cd1ce74a3b52ed47dc
raulsantiago <raulsantiago@id.uff.br>
2019-10-30 16:57:33 -0300

    Refatoração de todos testes de User

---
+++ XUnitTestAlfa/Builder/TopicBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Domain;
+
+public class TopicBuilder
+{
+    public Guid Id;
+    public string Name;
+
+    public static TopicBuilder New()
+    {
+        return new TopicBuilder()
+        {
+            Id = Guid.NewGuid(),
+            Name = "TestTopic",
+        };
+    }
+
+    public TopicBuilder WithId(Guid id)
+    {
+        Id = id;
+        return this;
+    }
+
+    public TopicBuilder WithName(string name)
+    {
+        Name = name;
+        return this;
+    }
+
+    public Topic Build()
+    {
+        return new Topic(Id, Name);
+    }
+}
+
