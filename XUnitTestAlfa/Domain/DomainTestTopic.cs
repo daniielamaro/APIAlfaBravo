f37e38fd8f0666334b7d3134eb772b2b1d0f4428
John-91 <jonvaladao91@gmail.com>
2019-10-30 17:59:55 -0300

    atualização

---
+++ XUnitTestAlfa/Domain/DomainTestTopic.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestAlfa.Domain
+{
+    public class DomainTestTopic
+    {
+        [Fact]
+        public void TestCreate()
+        {
+            Topic topic = TopicBuilder.New().WithName("Nome teste").Build();
+
+            Assert.True(topic.Id != Guid.Empty && topic.Id != null);
+            Assert.True(topic.Name == "Nome teste");
+        }
+    }
+}
